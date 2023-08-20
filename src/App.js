389803898aaf49dcfe1e6af3cef7b44f8986c79b
Nata Ponamarenko <nuts08air@inbox.ru>
2023-08-20 23:22:12 +0800

    change accept

--- src/App.js
+++ src/App.js
@@ -1,125 +1,24 @@
 import './App.css';
-import dangerGroup from './untils/dangerGroup';
-import danger from './untils/danger';
-import prof from './untils/prof';
-import dangerEvent from './untils/dangerousEvent';
-import Select from 'react-select';
-import * as Excel from 'exceljs/dist/exceljs.min.js';
-import { useEffect, useState } from 'react';
-import Footer from './Footer';
+import { Route, Routes } from 'react-router-dom';
+import Form from './components/Form/Form';
+import Update from './components/Update/Update';
+import Header from './components/Header/Header';
+import ModalUpdata from './components/ModalUpdata/ModalUpdata';
+import { useState } from 'react';
 
 function App() {
-
-  const [isModal, setModal] = useState(false)
+  const [isModal, setModal] = useState(false);
   return (
     <div className='App'>
-      <main className='main'>
-        <form className='form' onSubmit={handleSubmit} required>
-          <div className='lable-wrapper'>
-            <label className='lable'>
-              Профессии:
-              <Select
-                className='react-select-container'
-                classNamePrefix='react-select'
-                options={prof}
-                onChange={(evt) => console.log(evt)}
-                placeholder={'Профессии'}
-              />
-            </label>
-            <label className='lable'>
-              Группа опасности:
-              <Select
-                className='react-select-container'
-                classNamePrefix='react-select'
-                options={dangerGroup}
-                onChange={(name) => clearDangerGroup(name)}
-                required
-                placeholder={'Группа опасности'}
-                value={selectedOption}
-              />
-            </label>
-            <label className='lable'>
-              Опасности:
-              <Select
-                className='react-select-container'
-                classNamePrefix='react-select'
-                options={isArr}
-                onChange={(evt) => clearDanger(evt)}
-                isDisabled={disabled}
-                required
-                placeholder={'Опасности'}
-                value={selectedOptionDanger}
-              />
-            </label>
-            <label className='lable'>
-              Опасное событие
-              <Select
-                className='react-select-container'
-                classNamePrefix='react-select'
-                options={isDangerEventArr}
-                onChange={(evt) => clearDangerEvent(evt)}
-                required
-                placeholder={'Опасное событие'}
-                value={selectedOptionDangerEvt}
-              />
-            </label>
-            <label className='lable box'>
-              Тяжесть
-              <input
-                className='form__input input'
-                onChange={(evt) => setProbability(evt.target.value)}
-                required
-              ></input>
-            </label>
-            <label className='lable box'>
-              Вероятность
-              <input
-                className='form__input input'
-                onChange={(evt) => setHeaviness(evt.target.value)}
-                required
-              ></input>
-            </label>
-          </div>
-          <div className='form__container'>
-            <div className='wrapper'>
-              <span className='wrapper_text'>ИПР: {ipr}</span>
-              <span className='wrapper_text'>Уровень риска: {risk}</span>
-              <span className='wrapper_text'>
-                Приемлемость: {acceptability}
-              </span>
-              <span className='wrapper_text'>
-                Отношение к риску: {riskAttitude}
-              </span>
-            </div>
-          </div>
-          <div className='lable-wrapper'>
-            <label className='lable'>
-              Объект
-              <input
-                className='form__input standart'
-                autoComplete='on'
-                onChange={(evt) => setObj(evt.target.value)}
-              ></input>
-            </label>
-            <label className='lable'>
-              Источник
-              <input
-                className='form__input standart'
-                autoComplete='on'
-                onChange={(evt) => setSource(evt.target.value)}
-              ></input>
-            </label>
-          </div>
-          <div className='buttons_wrapper'>
-            <input type='submit' className='btn send'></input>
-            <input type='reset' className='btn reset' onClick={clear}></input>
-          </div>
-        </form>
-        <button onClick={table} className='btn button__table'>
-          Выгрузить в таблицу
-        </button>
-      </main>
-      <Footer />
+      <ModalUpdata active={isModal} setModal={setModal}></ModalUpdata>
+      <Header></Header>
+      <Routes>
+        <Route path='/techoavia-roman' element={<Form></Form>}></Route>
+        <Route
+          path='/update'
+          element={<Update setModal={setModal}></Update>}
+        ></Route>
+      </Routes>
     </div>
   );
 }
