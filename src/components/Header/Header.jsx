9d5b7d2957d1c42b6b33173110c865c8e2f3a0c8
Nata Ponamarenko <nuts08air@inbox.ru>
2023-08-26 01:15:14 +0800

    изменение стилей header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,23 @@
+import { useState } from 'react';
 import { NavLink } from 'react-router-dom';
-import './Header.css'
+import './Header.css';
 
 function Header() {
+  const [active, setActive] = useState(false);
   return (
     <header className='header'>
-      <NavLink to='/techoavia-roman'>Форма</NavLink>
-      <NavLink to='/update'>Обновление информации</NavLink>
+      <NavLink
+        to='/techoavia-roman'
+        className={active ? 'header__text active' : 'header__text'}
+      >
+        Форма
+      </NavLink>
+      <NavLink
+        to='/update'
+        className={active ? 'header__text active' : 'header__text'}
+      >
+        Обновление информации
+      </NavLink>
     </header>
   );
 }
